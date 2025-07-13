01135289236418fa28bb71b1ba54cb5945cf50ea
Alrainn <melihawci@gmail.com>
2025-07-13 19:11:04 +0300

    feat: define IMovieService interface with CRUD method signatures

--- src/main/java/com/melihawci/services/IMovieService.java
+++ src/main/java/com/melihawci/services/IMovieService.java
@@ -1,5 +1,21 @@
 package com.melihawci.services;
 
-public interface IMovieService {
+import java.util.List;
+import java.util.Optional;
 
+import com.melihawci.dto.MovieRequestDTO;
+import com.melihawci.dto.MovieResponseDTO;
+
+public interface IMovieService 
+{
+
+	public MovieResponseDTO save(MovieRequestDTO movieRequestDTO);
+	
+	public Optional<MovieResponseDTO> findById(Long id);
+	
+	public List<MovieResponseDTO> findAll();
+	
+	public MovieResponseDTO update(MovieRequestDTO movieRequestDTO);
+	
+	public void deleteById(Long id);
 }
