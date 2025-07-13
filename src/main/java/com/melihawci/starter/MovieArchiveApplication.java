124c8cfd99db28691e54b451417fada89dd6c5e6
Alrainn <melihawci@gmail.com>
2025-07-13 19:08:36 +0300

    chore: add @ComponentScan, @EnableJpaRepositories, and @EntityScan annotations to main application class

--- src/main/java/com/melihawci/starter/MovieArchiveApplication.java
+++ src/main/java/com/melihawci/starter/MovieArchiveApplication.java
@@ -2,7 +2,14 @@ package com.melihawci.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@ComponentScan(basePackages = {"com.melihawci"})
+@EnableJpaRepositories(basePackages = {"com.melihawci"})
+@EntityScan(basePackages = {"com.melihawci"})
 @SpringBootApplication
 public class MovieArchiveApplication {
 
