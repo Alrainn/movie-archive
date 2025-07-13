1576b40a9e846e68bcec70918e38724d1dc4773f
Alrainn <melihawci@gmail.com>
2025-07-13 16:44:34 +0300

    feat: add Movie entity class with basic fields and JPA annotations

---
+++ src/main/java/com/melihawci/entities/Movie.java
@@ -0,0 +1,43 @@
+package com.melihawci.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "movies")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie 
+{
+	
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "title", nullable = false)
+	private String title;
+	
+	@Column(name = "director", nullable = false)
+	private String director;
+	
+	@Column(name = "release_year", nullable = false)
+	private int releaseYear;
+	
+	@Column(name = "genre", nullable = false)
+	private String genre;
+	
+	@Column(name = "rating", nullable = false)
+	private double rating;
+	
+	
+}
