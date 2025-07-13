56e0aaee1b9623eecb98e66fd77dd00c99c150b6
Alrainn <melihawci@gmail.com>
2025-07-13 19:10:28 +0300

    chore: add @Repository annotation to MovieRepository interface

--- src/main/java/com/melihawci/repository/MovieRepository.java
+++ src/main/java/com/melihawci/repository/MovieRepository.java
@@ -1,9 +1,11 @@
 package com.melihawci.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.melihawci.entities.Movie;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, Long>{
 
 }
