76f08025149fd498f9850dd4571c343d20208726
Alrainn <melihawci@gmail.com>
2025-07-13 16:55:26 +0300

    refactor: rename MovieArchiveApplication package and move class to 'starter' package

--- src/main/java/com/melihawci/starter/MovieArchiveApplication.java
+++ src/main/java/com/melihawci/starter/MovieArchiveApplication.java
@@ -1,4 +1,4 @@
-package com.melihawci.movie_archive;
+package com.melihawci.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
